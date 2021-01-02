d63356ff2b102c1ab92343cd80b128932d90faa8
Pascal Schröder <paschroeder@uni-osnabrueck.de>
2021-01-02 18:25:06 +0100

    call the implausible model

--- color_critter.py
+++ color_critter.py
@@ -31,4 +31,4 @@ ANOTHER_MAP="""
 color_world = ColorWorld(MY_MAP)
 # Your model might not be a nengo.Netowrk() - SPA is permitted
 model = nengo.Network()
-setup_naive_model(model, color_world=color_world)
+setup_implausible_model(model, color_world=color_world)
